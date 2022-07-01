8854600a9a659602f0f4a0098874fd252e4bc45a
puppetup <savinohoshizaki@gmail.com>
2022-06-30 19:45:54 -0700

    added switch case

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mysql = require('mysql2')
 const inquirer = require('inquirer');
-const { default: test } = require('node:test');
 
-connection.query();
+
+// connection.query();
 
 // Function that stores inquirer questions
 const promptUser = () => {
@@ -11,17 +11,48 @@ const promptUser = () => {
         {
             type: 'list',
             name: 'function',
-            choices: ['View all Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+        },
+
+    ])
+    .then((answer) => {
+        switch (answer.choice) {
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateRole();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Quit':
+                break;
+                
         }
-    ]);
+    });
 };
 
+
+
 // create connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: test
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'employee_db',
+// });
 
 
 
